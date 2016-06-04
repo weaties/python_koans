4c9b5a5d9206c62a3b8d379d047e67b54b28007a
Daniel Weatbrook <weaties@gmail.com>
2016-06-04 07:00:49 -0700

    putting my changes in there

--- python2/koans/about_triangle_project.py
+++ python2/koans/about_triangle_project.py
@@ -22,3 +22,5 @@ class AboutTriangleProject(Koan):
         self.assertEqual('scalene', triangle(3, 4, 5))
         self.assertEqual('scalene', triangle(10, 11, 12))
         self.assertEqual('scalene', triangle(5, 4, 2))
+
+    
--- python2/koans/triangle.py
+++ python2/koans/triangle.py
@@ -19,7 +19,18 @@
 #
 def triangle(a, b, c):
     # DELETE 'PASS' AND WRITE THIS CODE
-    pass
+    if (a<=0) or (b <=0) or (c <=0):
+        raise TriangleError
+    
+    if (a+b <= c) or (a+c <= b) or (b+c <= a):
+        raise TriangleError
+    
+    if a == b == c:
+        return 'equilateral'
+    elif (a ==b) or (a==c) or (b ==c):
+        return 'isosceles'
+    else:
+        return 'scalene'
 
 
 # Error class used in part 2.  No need to change this code.
